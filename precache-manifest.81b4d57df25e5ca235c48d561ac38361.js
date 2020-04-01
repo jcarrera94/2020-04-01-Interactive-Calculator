1d366f92ca24d6262210a8ecf9e8cd7f8183df42
Juan Carrera <juankarrera@gmail.com>
2020-04-01 14:58:46 -0400

    Updates

---
+++ precache-manifest.81b4d57df25e5ca235c48d561ac38361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57f225b25688bc5aadf0b2f3fff677c",
+    "url": "/2020-04-01-Interactive-Calculator/index.html"
+  },
+  {
+    "revision": "0a4b53337315585fb95d",
+    "url": "/2020-04-01-Interactive-Calculator/static/css/main.c1bedb41.chunk.css"
+  },
+  {
+    "revision": "7d16289a4a1456e41b4e",
+    "url": "/2020-04-01-Interactive-Calculator/static/js/2.a3bbe033.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/2020-04-01-Interactive-Calculator/static/js/2.a3bbe033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4b53337315585fb95d",
+    "url": "/2020-04-01-Interactive-Calculator/static/js/main.aeef7ff9.chunk.js"
+  },
+  {
+    "revision": "f641dd1d7fa4f4968517",
+    "url": "/2020-04-01-Interactive-Calculator/static/js/runtime-main.09153aec.js"
+  }
+]);
